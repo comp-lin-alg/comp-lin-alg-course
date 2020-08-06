9cf7c28909720614ca63e6c80b2495bd7b7abb5e
Colin Cotter <colin.cotter@imperial.ac.uk>
2020-08-06 17:21:12 +0100

    made a test for the rank2 matrix

--- cla_utils/__init__.py
+++ cla_utils/__init__.py
@@ -1,1 +1,3 @@
 from cla_utils.exercises1 import basic_matvec, column_matvec, time_matvecs
+from cla_utils.exercises1 import 
+
--- test/test_exercises1.py
+++ test/test_exercises1.py
@@ -17,6 +17,25 @@ def test_basic_matvec(m, n):
 
     assert(np.linalg.norm(b-b0)<1.0e-6)
 
+# Test the basic matvec
+@pytest.mark.parametrize('m, n', [(20, 20), (40, 20), (20, 45)])
+def test_rank2_matrix(m, n):
+    random.seed(1451*m + 1901*n)
+    u1 = random.randn(m)
+    u2 = random.randn(m)
+    v1 = random.randn(n)
+    v2 = random.randn(n)
+
+    A = rank2(u1, u2, v1, v2)
+    a1 = random.randn(m)
+    a2 = random.randn(n)
+
+    n1 = np.vdot(a1, A.dot(a2))
+    n2 = np.vdot(a1, u1)*np.vdot(v1, a2) + \
+        np.vdot(a1, u2)*np.vdot(v2, a2)
+
+    assert(np.abs(n1-n2)<1.0e-7)
+
 
 if __name__ == '__main__':
     import sys

6596ef7bc78b3810dff740a0b02e2ecbda38a66e
Colin Cotter <colin.cotter@imperial.ac.uk>
2020-08-06 12:56:25 +0100

    created first two skeleton examples

---
+++ cla_utils/exercises1.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def basic_matvec(A, x):
+    """
+    Elementary matrix-vector multiplication.
+
+    :param A: an mxn-dimensional numpy array
+    :param x: an n-dimensional numpy array
+
+    returns an m-dimensional numpy array which is the product of A with x
+
+    This should be implemented using a double loop over the entries of A
+    """
+
+    pass
+
+
+def column_matvec(A, x):
+    """
+    Matrix-vector multiplication using the representation of the product
+    Ax as linear combinations of the columns of A, using the entries in 
+    x as coefficients.
+
+
+    :param A: an mxn-dimensional numpy array
+    :param x: an n-dimensional numpy array
+
+    returns an m-dimensional numpy array which is the product of A with x
+
+    This should be implemented using a single loop over the entries of x
+    """

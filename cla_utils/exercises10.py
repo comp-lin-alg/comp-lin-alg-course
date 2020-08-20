96488be6c17ec1614f0102b3e9318d06e1305546
Colin Cotter <colin.cotter@imperial.ac.uk>
2020-08-20 17:53:01 +0100

    matrix readers

--- cla_utils/exercises10.py
+++ cla_utils/exercises10.py
@@ -46,4 +46,20 @@ def GMRES(A, b, maxit, tol, x0=None, return_residual_norms=False,
     
     raise NotImplementedError
 
-def get_AA100
+
+def get_AA100():
+    AA100 = np.fromfile('AA100.dat', sep=' ')
+    AA100 = AA100.reshape((100, 100))
+    return AA100
+
+
+def get_BB100():
+    BB100 = np.fromfile('BB100.dat', sep=' ')
+    BB100 = BB100.reshape((100, 100))
+    return BB100
+
+
+def get_CC100():
+    CC100 = np.fromfile('CC100.dat', sep=' ')
+    CC100 = CC100.reshape((100, 100))
+    return CC100

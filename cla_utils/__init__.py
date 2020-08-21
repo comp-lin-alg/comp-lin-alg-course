e359b19c64b825c227845a97ffb691bc8a719303
Colin Cotter <colin.cotter@imperial.ac.uk>
2020-08-21 13:54:33 +0100

    just import * from each exercise

--- cla_utils/__init__.py
+++ cla_utils/__init__.py
@@ -1,12 +1,10 @@
-from cla_utils.exercises1 import basic_matvec, column_matvec, time_matvecs
-from cla_utils.exercises1 import rank2, rank1pert_inv, ABiC
-from cla_utils.exercises2 import orthog_cpts, solveQ, orthog_proj
-from cla_utils.exercises2 import orthog_space, GS_modified, GS_classical
-from cla_utils.exercises2 import GS_modified_get_R
-from cla_utils.exercises3 import householder, householder_solve
-from cla_utils.exercises3 import householder_qr, householder_ls
-from cla_utils.exercises4 import operator_2_norm, cond
-from cla_utils.exercises5 import randomQ, randomR
-from cla_utils.exercises5 import solve_R, backward_stability_householder
-from cla_utils.exercises6 import LU_inplace, get_Lk, solve_L, solve_U
-from cla_utils.exercises9 import pow_it
+from cla_utils.exercises1 import *
+from cla_utils.exercises2 import *
+from cla_utils.exercises3 import *
+from cla_utils.exercises4 import *
+from cla_utils.exercises5 import *
+from cla_utils.exercises6 import *
+from cla_utils.exercises7 import *
+from cla_utils.exercises8 import *
+from cla_utils.exercises9 import *
+from cla_utils.exercises10 import *

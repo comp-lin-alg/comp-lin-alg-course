2d317073288e52765a582b47daa8f8f8a35ae778
ThomasGregory <tjg2118@ic.ac.uk>
2020-09-29 13:40:11 +0100

    started work on tests 6

--- test/test_exercises4.py
+++ test/test_exercises4.py
@@ -7,11 +7,11 @@ import numpy as np
 
 @pytest.mark.parametrize('m, n', [(20, 7), (40, 13), (9, 87)])
 def test_operator_2_norm(m, n):
-    random.seed(8473*m + 9283*k)
+    random.seed(8473*m + 9283*n)
     A = random.randn(m, n)
 
     norm1 = cla_utils.operator_2_norm(A)
-    u, s, v = linalg.svd(A)
+    u, s, v = np.linalg.svd(A)
     norm2 = s[0]
 
     assert(np.abs(norm1 - norm2) < 1.0e-6)
@@ -19,10 +19,10 @@ def test_operator_2_norm(m, n):
 @pytest.mark.parametrize('m', [20, 40, 233])
 def test_cond(m):
     random.seed(3168*m)
-    A = random.randn(m)
+    A = random.randn(m, m)
 
     cond1 = cla_utils.cond(A)
-    cond2 = numpy.linalg.cond(A)
+    cond2 = np.linalg.cond(A)
 
     assert(np.abs(cond1 - cond2) < 1.0e-6)
 

6d857221e57c945cf635c2a13b462eac8df19ee0
Bensong0506 <wenxuan.song18@imperial.ac.uk>
2020-10-10 01:01:59 +0800

    Exercise one

--- cla_utils/exercises1.py
+++ cla_utils/exercises1.py
@@ -21,6 +21,13 @@ def basic_matvec(A, x):
 
     :return b: m-dimensional numpy array
     """
+    b = np.empty(len(A))
+    for i in range(len(A)):
+        sum=0
+        for j in range(len(A[0])):
+            sum += A[i][j]*x[j]
+            b[i] = sum
+    return b
 
     raise NotImplementedError
 
@@ -39,6 +46,12 @@ def column_matvec(A, x):
 
     This should be implemented using a single loop over the entries of x
     """
+    b = 0
+    
+    for i in range(len(x)):
+        b = b + A[:,i]*x[i]
+    
+    return b
 
     raise NotImplementedError
 
@@ -92,12 +105,14 @@ def rank2(u1, u2, v1, v2):
     :param v1: n-dimensional numpy array
     :param v2: n-dimensional numpy array
     """
-
-    raise NotImplementedError
-
+    
+    B = np.column_stack( [ u1 , u2] )
+    C = np.array([v1.conjugate(), v2.conjugate()])
     A = B.dot(C)
-
     return A
+
+    
+
 
 
 def rank1pert_inv(u, v):
@@ -108,10 +123,16 @@ def rank1pert_inv(u, v):
     :param u: m-dimensional numpy array
     :param v: m-dimensional numpy array
     """
+    u = u.reshape(len(u),1)
+    v = v.reshape(1,len(v))
+    v_c = v.conjugate()
+    
+    Ainv = np.identity(len(u))-(u @ v_c)/(1+v_c @ u)
+    
+    return Ainv
 
     raise NotImplementedError
 
-    return Ainv
 
 
 def ABiC(Ahat, xr, xi):
@@ -124,7 +145,45 @@ def ABiC(Ahat, xr, xi):
     :return zr: m-dimensional numpy arrays containing the real part of z.
     :return zi: m-dimensional numpy arrays containing the imaginary part of z.
     """
-
-    raise NotImplementedError
-
+    B=np.zeros((len(Ahat),len(Ahat)))
+    C=np.zeros((len(Ahat),len(Ahat)))
+    for i in range(len(Ahat)):
+        for j in range(len(Ahat[0])):
+            if i<j:
+                C[i][j] = Ahat[i][j]
+                C[j][i] = -Ahat[i][j]
+            elif i==j:
+                B[i][j] = Ahat[i][j]
+                C[i][j] = 0
+            else :
+                B[i][j] = Ahat[i][j]
+                B[j][i] = Ahat[i][j]
+    
+    A = B+1j*C
+    z = A@(xr+1j*xi)
+    zr = z.real
+    zi = z.imag
     return zr, zi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
